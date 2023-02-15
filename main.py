dbe7cc8f7d23aff480d0ae24d5aa416b53b3f381
MinHyeok <4438mh@naver.com>
2023-02-15 18:51:49 +0900

    Day 2

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from tkinter import *
-
-win = Tk() # 창 생성
-
-win.title("탄력적 습관") # 창 제목
-win.geometry("1366x768") # 창 크기 가로x세로
-win.option_add("*Font", "맑은고딕 25")
-
-btn = Button(win, text="버튼")
-btn.pack()
-
-win.mainloop() # 창 실행
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel, QLineEdit, QTextEdit
+from PyQt5.QtGui import QIcon # 아이콘 넣기
+from PyQt5.QtCore import QCoreApplication
 
 
+# 작은 창 하나 띄우기
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        grid.addWidget(QLabel('날짜'), 0, 0)
+        grid.addWidget(QLabel('독서하기'), 1, 0)
+        grid.addWidget(QLabel('운동하기'), 2, 0)
+        grid.addWidget(QLabel('글쓰기'), 3, 0)
+
+        self.setWindowTitle('Elastic Habits')  # 창의 제목
+        self.setWindowIcon(QIcon('icon.png'))  # 아이콘 넣기
+        self.setGeometry(300, 300, 1366, 768)  # 창의 위치(앞에 2개)와 크기(뒤에 2개) move + resize 합친 것과 같음
+        # self.move(300, 300)  # 위젯을 스크린의 x=300px, y=300px (아직 모르겠음)
+        # self.resize(400, 200)  # 위젯의 크기
+        self.show()
 
 
-# # 습관
-# def habits():
-#     pass
-#
-# input("사용하실 습관 3가지를 입력해주세요 :")
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
